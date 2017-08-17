b61b4d06815488ab3da889496d371e381a1317fa
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2017-08-17 23:36:36 +0200

    add leader board

---
+++ leaderboard/js/firebase-key.js
@@ -0,0 +1,8 @@
+var cs = { 
+    apiKey: "AIzaSyDUnzomJ2l9pddOSFUTXbEe0gSXv1WtoMg",
+    authDomain: "leaderboard-67154.firebaseapp.com",
+    databaseURL: "https://leaderboard-67154.firebaseio.com",
+    projectId: "leaderboard-67154",
+    storageBucket: "",
+    messagingSenderId: "261317736653"
+};
