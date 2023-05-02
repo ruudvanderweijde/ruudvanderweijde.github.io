df289559bdfa6be149686aaf0111cff2d5ba84dd
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2023-05-02 21:46:14 +0200

    Updates

---
+++ precache-manifest.6cb63a61c4ac14cf293c7b366a486423.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3ba186c0e85cd2d5bf1",
+    "url": "/static/js/main.f3ba186c.chunk.js"
+  },
+  {
+    "revision": "85f2ca843b994b3fcff8",
+    "url": "/static/css/1.b087c04d.chunk.css"
+  },
+  {
+    "revision": "85f2ca843b994b3fcff8",
+    "url": "/static/js/1.85f2ca84.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "98afd357e22f54d4278b443b3480bfcb",
+    "url": "/index.html"
+  }
+];
